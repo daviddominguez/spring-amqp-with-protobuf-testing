1399007d175e45691edca920ff238474e56fbe3e
David Dominguez <david.dominguez@amplia.es>
2016-08-02 14:56:20 +0200

    test(spring-amqp-with-protobuf-testing): update tests to send domain model objects
    ...instead protobuf objects.

--- src/test/java/es/amplia/springamqp/SpringAmqpApplicationTests.java
+++ src/test/java/es/amplia/springamqp/SpringAmqpApplicationTests.java
@@ -2,47 +2,73 @@ package es.amplia.springamqp;
 
 import com.google.protobuf.Message;
 import es.amplia.springamqp.configuration.SpringAmqpConfiguration;
+import es.amplia.springamqp.model.AuditMessage;
+import es.amplia.springamqp.model.AuditMessageNorth;
+import es.amplia.springamqp.model.AuditMessageSouth;
+import es.amplia.springamqp.model.builder.AuditMessageNorthBuilder;
+import es.amplia.springamqp.model.builder.AuditMessageSouthBuilder;
+import es.amplia.springamqp.model.serializer.ProtobufSerializer;
 import es.amplia.springamqp.protobuf.AuditMessageProtobuf;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.Collections;
+import java.util.UUID;
 
 public class SpringAmqpApplicationTests extends AbstractSpringBootTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringAmqpApplicationTests.class);
 
     @Autowired
-    private AmqpTemplate amqpTemplate;
+    private RabbitTemplate amqpTemplate;
+
+    @Autowired
+    @Qualifier("auditMessageNorthConverter")
+    private MessageConverter auditMessageNorthConverter;
+
+    @Autowired
+    @Qualifier("auditMessageSouthConverter")
+    private MessageConverter auditMessageSouthConverter;
 
     @Test
     public void test () {
-        Message messageNorth = AuditMessageProtobuf.AuditMessageNorthProtobuf.newBuilder()
-                .setComponent("component")
-                .setName("name")
-                .setType(AuditMessageProtobuf.AuditMessageNorthProtobuf.MsgType.REQUEST)
-                .setDirection(AuditMessageProtobuf.AuditMessageNorthProtobuf.MsgDirection.OUT)
-                .setByteSize(100)
-                .setTransactionId("transactionId")
-                .setStatus(AuditMessageProtobuf.AuditMessageNorthProtobuf.MsgStatus.SUCCESS)
-                .setSubject("subject")
+        AuditMessageNorth messageNorth = (AuditMessageNorth) AuditMessageNorthBuilder.builder()
+                .userId(UUID.randomUUID())
+                .component("component")
+                .name("name")
+                .type(AuditMessage.MsgType.REQUEST)
+                .direction(AuditMessage.MsgDirection.OUT)
+                .byteSize(100)
+                .transactionId("transactionId")
+                .status(AuditMessage.MsgStatus.SUCCESS)
+                .subject("subject")
+                .payload(Collections.<String, String>emptyMap())
                 .build();
+
         LOGGER.debug("Sending message '{}'", messageNorth);
+        amqpTemplate.setMessageConverter(auditMessageNorthConverter);
         amqpTemplate.convertAndSend(SpringAmqpConfiguration.EXCHANGE, SpringAmqpConfiguration.NORTH_MESSAGE_ROUTING_KEY, messageNorth);
 
-        Message messageSouth = AuditMessageProtobuf.AuditMessageSouthProtobuf.newBuilder()
-                .setComponent("component")
-                .setName("name")
-                .setType(AuditMessageProtobuf.AuditMessageSouthProtobuf.MsgType.REQUEST)
-                .setDirection(AuditMessageProtobuf.AuditMessageSouthProtobuf.MsgDirection.OUT)
-                .setByteSize(100)
-                .setTransactionId("transactionId")
-                .setStatus(AuditMessageProtobuf.AuditMessageSouthProtobuf.MsgStatus.SUCCESS)
-                .setSubject("subject")
+        AuditMessageSouth messageSouth = (AuditMessageSouth) AuditMessageSouthBuilder.builder()
+                .deviceId("deviceId")
+                .component("component")
+                .name("name")
+                .type(AuditMessage.MsgType.REQUEST)
+                .direction(AuditMessage.MsgDirection.OUT)
+                .byteSize(100)
+                .transactionId("transactionId")
+                .status(AuditMessage.MsgStatus.SUCCESS)
+                .subject("subject")
+                .payload(Collections.<String, String>emptyMap())
                 .build();
         LOGGER.debug("Sending message '{}'", messageSouth);
+        amqpTemplate.setMessageConverter(auditMessageSouthConverter);
         amqpTemplate.convertAndSend(SpringAmqpConfiguration.EXCHANGE, SpringAmqpConfiguration.SOUTH_MESSAGE_ROUTING_KEY, messageSouth);
-
     }
 }
